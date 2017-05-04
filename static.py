ffc509a31c1f23f152f4d27b037d15c7215aa69e
LonnieCoffman <LonnieCoffman@users.noreply.github.com>
2017-05-03 17:30:41 -0700

    Update static.py

--- static.py
+++ static.py
@@ -1,5 +1,5 @@
 import oandapy
 
-account_id = 745676
+account_id = 123456
 filepath = "D:\\OANDA - MetaTrader - Demo\\mql4\\Files\\FXtrade\\"
-oanda = oandapy.API(environment="live", access_token="0a8186c456c1250ec583e17f51fc30be-61d2265ad3393f78d9cea0c696104e72")
+oanda = oandapy.API(environment="live", access_token="")
